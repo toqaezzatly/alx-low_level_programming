985039c8dbf73fad7e7159b4263877c985b914e1
root <toqaezzatly@gmail.com>
2023-07-24 17:49:42 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string reversely
+ *
+ * @s: pointer
+ * Description: takes pointer to the  first letter in string
+ *
+ * Return - void -> prints
+*/
+
+void print_rev(char *s)
+{
+	int len;
+
+	s = s + len;
+	for (int i = 0; i <= 0; i++)
+		printf("%c", *(s--);
+}
