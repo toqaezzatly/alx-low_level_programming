0420c8742b8ee77958205ef52c84b257b4a31466
root <toqaezzatly@gmail.com>
2023-07-24 19:04:37 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] = '\0'; i++)
 	{
-		_putchar(*str);
-		str = str + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 
