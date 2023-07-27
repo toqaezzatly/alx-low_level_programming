4717515e035b3c015e84181f984ce6dbc905cb65
root <toqaezzatly@gmail.com>
2023-07-27 02:06:41 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - fn to overwrite '\0' of dest
+ * with 1st char of src
+ * Description: takes names of the arrays of strings aka
+ * pointers to the 1st elements of strings
+ *
+ *
+ * @dest: pointer
+ *
+ * @src: ..
+ *
+ * Return: pointer to dest string
+ *
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
