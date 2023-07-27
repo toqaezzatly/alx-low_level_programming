b19e193b46a4aa8c980211f2d4f5cfa340bbaad3
root <toqaezzatly@gmail.com>
2023-07-27 16:17:33 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,28 @@
 #include "main.h"
+
 /**
- * _strcat - fn to overwrite '\0' of dest
- * with 1st char of src
- * Description: takes names of the arrays of strings aka
- * pointers to the 1st elements of strings
- *
- *
- * @dest: pointer
- *
- * @src: ..
- *
- * @n: number of bytes
- *
- * Return: pointer to dest string
- *
-*/
-char *_strncat(char *dest, char *src, int n);
+ * _strncat - concatenate n bytes to another string
+ * @dest: destination
+ * @src: source
+ * @n: append n bytes
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int dest_len;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
-		dest_len++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+		i++;
+
+	while (src[j] != src[n])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
 	return (dest);
 }
