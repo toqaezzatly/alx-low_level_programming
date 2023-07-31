3ba9771284794de882da7b60177a0c2a7445c5a9
root <toqaezzatly@gmail.com>
2023-07-31 06:25:04 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,2 +1,20 @@
-:wq
+#include "main.h"
 
+/**
+ * _memset - fn used to allocate specific block in mem
+ * @s: 1st location
+ * @b: value to stored
+ *
+ *@n: number of memory location
+ *
+ * Return: adrs of 1st location
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*s++ = b;
+	return (s);
+}
