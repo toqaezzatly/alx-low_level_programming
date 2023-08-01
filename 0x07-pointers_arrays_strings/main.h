5cbf55460cbac42bb571476eb54eda9c5d8dd5c9
root <toqaezzatly@gmail.com>
2023-07-31 20:00:29 -0700

    all

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-*_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
