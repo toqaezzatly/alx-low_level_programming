d04c18f8249c645a77628b4ecab2bca523cc686f
root <toqaezzatly@gmail.com>
2023-08-03 19:52:02 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power
+ * @x: base
+ * @y: power
+ * Return: x ^ y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
