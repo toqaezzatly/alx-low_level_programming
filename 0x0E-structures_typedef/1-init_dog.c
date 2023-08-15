94c58809c9a2e64a22dbca02997e14275400f00a
root <toqaezzatly@gmail.com>
2023-08-14 19:20:45 -0700

    0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * Initializes a dog struct with the given name, age, and owner.
+ *
+ * @param d A pointer to the dog struct to initialize.
+ * @param name The name of the dog.
+ * @param age The age of the dog.
+ * @param owner The owner of the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
