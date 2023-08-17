84aec8ab30d4e2247fd4ec4cbc562d3c0715b349
root <toqaezzatly@gmail.com>
2023-08-17 06:27:52 -0700

    commiting

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
-#include "stdarg.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
