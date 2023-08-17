9f68d23844f57d1dfe338910f10cc40d33d4038b
root <toqaezzatly@gmail.com>
2023-08-17 06:16:55 -0700

    commiting

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  sum_them_all - sum all input no matter how many inputs
+ * @n: variable name
+ * Return: sum
+*/
+int sum_all_them(const unsigned int n, ...)
+{
+	va_list args; /* pointer to list */
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (unsigned int i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
