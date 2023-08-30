31f0f3f8af251f0eb966809479eb7353f8b4864c
root <toqaezzatly@gmail.com>
2023-08-30 15:46:57 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * _sqrt - helper function
- * @n: number to be square rooted
- * @val: check ^ 2
- *
- * Return: val
+* _sqrt - helper function
+* @n: number to be square rooted
+* @val: check ^ 2
+*
+* Return: val
 */
 int _sqrt(int n, int val)
 {
-        if (val * val == n)
-                return (val);
-        if (val * val < n)
-                return (_sqrt(n, val + 1));
-        else
-                return (-1);
+if (val * val == n)
+return (val);
+if (val * val < n)
+return (_sqrt(n, val + 1));
+else
+return (-1);
 }
 /**
- * _sqrt_recursion - sqrt of num
- * @n: number
- *
- * Return: -1 -> no sqrt
+* _sqrt_recursion - sqrt of num
+* @n: number
+*
+* Return: -1 -> no sqrt
 */
 int _sqrt_recursion(int n)
 {
-        return (_sqrt(n, 1));
+return (_sqrt(n, 1));
 }
