7ff7ad528a8f60ec12b52259f57098ca7a08de55
root <toqaezzatly@gmail.com>
2023-08-30 09:49:18 -0700

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: pointer to array of strings
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == NULL)
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s - 1));
+	}
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+/*0x08-recursion*/
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
