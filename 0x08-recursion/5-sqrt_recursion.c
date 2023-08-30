995a5984cb5e81c3cec227181449f416a7034d51
root <toqaezzatly@gmail.com>
2023-08-30 15:40:39 -0700

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - sqrt of num
+ * @n: number
+ *
+ * Return: -1 -> no sqrt
+*/
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+/**
+ * sqrt - helper function
+ * @n: number to be square rooted
+ * @val: check ^ 2
+ *
+ * Return: val
+*/
+int sqrt(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	if (val * val < n)
+		return (sqrt(n, val + 1));
+	else
+		return (-1);
+}
