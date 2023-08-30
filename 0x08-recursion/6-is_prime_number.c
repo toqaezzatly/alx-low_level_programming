c263b4c80d784e880f570a3c47cf0cba724291b1
root <toqaezzatly@gmail.com>
2023-08-30 15:09:29 -0700

    commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* check_prime - real checker
+* @n: number to be checked
+* @divisor: helper arg
+*
+* Return: 0 | 1
+*/
+int check_prime(int n, int divisor)
+{
+if (divisor >= n && n > 1)
+return (1);
+if (n % divisor == 0 || n < 1)
+return (0);
+return (check_prime(n, divisor + 1));
+}
+/**
+* is_prime_number - checks if prime number
+* @n: number to be checked
+*
+* Return: 1 -> prime 0 -> not prime
+*/
+int is_prime_number(int n)
+{
+check_prime(n, 2);
+}
