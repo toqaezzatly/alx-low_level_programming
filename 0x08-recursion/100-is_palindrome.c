c09ce1178a0cefaf8c09cb789c711ca96d721fde
root <toqaezzatly@gmail.com>
2023-08-31 01:46:20 -0700

    commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * pal_helper - helper function
+ * @s: pointer
+ * @start: beginning
+ * @end: end
+ *
+ * Return: 1 if palindronme 0 if not
+*/
+int pal_helper(char *s, int start, int end)
+{
+	if (start == end)
+		return (1);
+	if (s[start] == s[end])
+		return (pal_helper(*s, start + 1, end - 1));
+	else
+		return (0);
+}
+/**
+ *  is_palindrome - checks palindrome
+ * @s: pointer
+ *
+ * Return: 1 -> pal 0 -> not
+*/
+int is_palindrome(char *s)
+{
+	return (pal_helper(s, 0, _strlen(s) - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -26,3 +26,5 @@ int factorial(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
