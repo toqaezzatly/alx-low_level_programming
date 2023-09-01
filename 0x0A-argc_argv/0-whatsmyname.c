c14db784d6c0e07de6202506984e0826841cf70b
root <toqaezzatly@gmail.com>
2023-09-01 12:08:30 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: size of array of argv
+ * @argv: array of arument passed to main fn
+ * Return: 0-> always
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
