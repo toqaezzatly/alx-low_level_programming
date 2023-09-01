da8760252de8958024bd05f5c346ee761982efa1
root <toqaezzatly@gmail.com>
2023-09-01 12:29:24 -0700

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: size of array of argv
+ * @argv: array of arument passed to main fn
+ * Return: 0-> always
+*/
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
