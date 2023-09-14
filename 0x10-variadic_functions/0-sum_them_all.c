96b8c79b3e03c78ed71dc182466b4a044b66865b
root <toqaezzatly@gmail.com>
2023-09-14 12:19:31 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "var_func.h"
+#include "variadic_functions.h"
 /**
  * sum_them_all - sum in varadic number of args
  * @n: number of args to summed
@@ -8,7 +8,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ptr;
-	int i = 0, sum = 0;
+	unsigned int i = 0, sum = 0;
 
 	if (n == 0)
 		return (0);
