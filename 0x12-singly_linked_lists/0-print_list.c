dd53ae620f8d73089bb13fe718a819bc7eae7ece
root <toqaezzatly@gmail.com>
2023-09-21 12:58:44 -0700

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,18 +5,17 @@
  *
  * Return: number of nodes
 */
-size_t print_list(const list_t *h)
+size_t print_list(const list_t *head)
 {
 	size_t count = 0;
-	const list_t *current = h;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		if (current->str == NULL)
+		if (head->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len_, h->str);
-		h = h->next;
+			printf("[%d] %s\n", head->len_, head->str);
+		head = head->next;
 		count++;
 	}
 	return (count);
