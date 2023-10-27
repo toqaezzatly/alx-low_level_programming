daa14f07f7068219a8f2284bd3fe08348c2dc36b
toqaezzatly <toqaezzatly@gmail.com>
2023-10-27 13:56:18 -0700

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,26 +4,26 @@
 
 int main(int argc, char *argv[])
 {
-    int i, j, sum;
-    
-    suum = 0;
+int i, j, sum;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
+sum = 0;
 
-        sum += atoi(argv[i]);
-    }
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
 
-    printf("%d\n", sum);
+sum += atoi(argv[i]);
+}
 
-    return 0;
+printf("%d\n", sum);
+
+return (0);
 }
 
