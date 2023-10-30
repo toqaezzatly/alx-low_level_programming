ab00d70777944c19d75da3b2849e023fed481702
toqaezzatly <toqaezzatly@gmail.com>
2023-10-30 05:02:49 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,44 +3,39 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: columns
- * @height: rows
+ * str_concat - concatenates two strings
+ * @s1: string1
+ * @s2: string2
  *
- * Return: Double Pointer
+ * Return: Pointer
  */
-int **alloc_grid(int width, int height)
+char *str_concat(char *s1, char *s2)
 {
-	int **s;
-	int i, j, k, fr;
+	int l1, l2, i, j;
+	char *s;
+	char *nul = "";
 
-	fr = 0;
-	if (width <= 0 || height <= 0)
-		return (0);
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
 
-	s = malloc(height * sizeof(int *));
+	l1 = 0, l2 = 0;
+	while (*(s1 + l1))
+		l1++;
+	while (*(s2 + l2))
+		l2++;
+
+	s = malloc(sizeof(char) * (l1 + l2 + 1));
+
 	if (s == 0)
 		return (0);
-	for (i = 0; i < height; i++)
-	{
-		*(s + i) = malloc(width * sizeof(int));
-		if (*(s + i) == 0)
-		{
-			fr = 1;
-			break;
-		}
-		for (j = 0; j < width; j++)
-		{
-			s[i][j] = 0;
-		}
-	}
-	if (fr == 1)
-	{
-		for (k = 0; k <= i; k++)
-		{
-			free(*(s + k));
-		}
-		free(s);
-	}
+
+	for (i = 0; i < l1; i++)
+		*(s + i) = *(s1 + i);
+
+	for (i = 0, j = l1; i <= l2; j++, i++)
+		*(s + j) = *(s2 + i);
+
 	return (s);
 }
