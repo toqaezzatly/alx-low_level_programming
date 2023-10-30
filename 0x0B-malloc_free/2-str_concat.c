493729738666b8e192aa087b079893edaca7e91b
toqaezzatly <toqaezzatly@gmail.com>
2023-10-30 04:55:03 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 /**
- * str_concat - concatenates 2 strings
- *
- * @s1: first string
- * @s2: string to add to end of of first string
- *
- * Return: pointer to newly allocated string concatenation
- */
+* str_concat - concatenates two strings
+* @s1: string1
+* @s2: string2
+*
+* Return: Pointer
+*/
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int size1 = 0, size2 = 0;
-	char *ptr, *ret;
+if (s1 == NULL) s1 = "";
 
-	ptr = s1;
-	if (s1)
-		while (*ptr++)
-			size1++;
-	else
-		s1 = "";
+if (s2 == NULL) s2 = "";
 
-	ptr = s2;
-	if (s2)
-		while (*ptr++)
-			size2++;
-	else
-		s2 = "";
+size_t len1 = strlen(s1);
+size_t len2 = strlen(s2);
 
-	ret = malloc(size1 + size2 + 1);
-	if (!ret)
-		return (NULL);
+char *s = malloc(len1 + len2 + 1);
+if (s == NULL) return (NULL);
 
-	ptr = ret;
-	while (*s1)
-		*ptr++ = *s1++;
-	while (*s2)
-		*ptr++ = *s2++;
-	*ptr = 0;
+memcpy(s, s1, len1);
+memcpy(s + len1, s2, len2 + 1);
 
-	return (ret);
+return (s);
 }
