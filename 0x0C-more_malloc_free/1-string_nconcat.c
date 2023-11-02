2ea1e7adf867f9fe7e105fc6cf88b5f269d5d42f
toqaezzatly <toqaezzatly@gmail.com>
2023-11-02 13:17:29 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,32 +8,38 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, newlen;
-	char *new_str;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-	if (s2 == NULL || n == 0)
-	{
-		s2 = "";
-	}
-	if (n < 0)
-	{
-		return (s1);
-	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	newlen = len1 + (n < len2 ? n : len2) + 1;
-	new_str = malloc(newlen);
 
-	if (new_str ==  NULL)
-	{
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	}
-	strcpy(new_str, s1);
-	strncpy(new_str + len1, s2, n);
-	new_str[newlen - 1] = '\0';
-	return (new_str);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
