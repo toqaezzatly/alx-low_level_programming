355543a07a04bf5eedac4db4e85d8bdb80116bc8
toqa <toqaezzatly@gmail.com>
2023-11-24 16:29:42 -0500

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,16 +11,16 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 	while (max)
 	{
 		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
+			putchar('0');
 		else if ((n & max) != 0)
 		{
-			_putchar('1');
+			putchar('1');
 			flag = 1;
 		}
 		max >>= 1;
