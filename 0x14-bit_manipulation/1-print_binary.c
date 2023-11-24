2f77e96121a219e36b3fa222910cb0c715ca5f6a
toqa <toqaezzatly@gmail.com>
2023-11-24 15:59:33 -0500

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,25 +5,24 @@
 */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
-	int leadingzeros = 1;
+	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1); /*1000 0000 0000 0000 0000 0000 0000 0000 */
+	int leadingzeros = 0;
 
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
 	while (mask)
 	{
-		if (n & mask)
-		{
-			if (leadingzeros)
-			{
-				leadingzeros = 0;
-			}
-			putchar('1');
-		}
-		else if (!leadingzeros)
-		{
+		if (leadingzeros == 1 && (n & mask) == 0)
 			putchar('0');
+		else if ((n & mask) != 0)
+		{
+			putchar('1');
+			leadingzeros = 1;
 		}
 		mask >>= 1;
 	}
 	putchar('\n');
 }
-
