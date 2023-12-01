268615438875e4fa16138d6f5b8a9ba85d40c268
toqa <toqaezzatly@gmail.com>
2023-12-01 12:36:52 -0500

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,32 @@
 #include "main.h"
+
 /**
- * create_file - creates file
- * @filename: filepath and extension
- * @text_content: string to be written in the file
- * Return: 1 if succeeded -1 if failed
-*/
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len;
+	int fd, w, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
-	for (len = 0; text_content[len];)
-		len++;
 
-	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
+
 	close(fd);
 
 	return (1);
