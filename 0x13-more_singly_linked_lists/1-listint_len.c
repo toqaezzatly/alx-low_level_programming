15257cc855c9831d6d4f4615fddf4bc637000fc4
toqa <toqaezzatly@gmail.com>
2023-12-06 18:32:08 -0500

    commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - print lenght of the linked list
+ * @h: ponter to the first node
+ *
+ * Return: size of list
+*/
+size_t listint_len(const listint_t *t)
+{
+	size_t i = 0;
+	
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
