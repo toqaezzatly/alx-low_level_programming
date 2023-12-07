97cdac5a06c7006a3d0396a20720f8dfb3155ca9
toqa <toqaezzatly@gmail.com>
2023-12-07 13:29:37 -0500

    commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - pops head node of list
+ * @head: address of pointer to first node
+ *
+ * Return: the value of the popped node
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
