f23a19875efe7201573a88644eb3f8fe721511fc
root <toqaezzatly@gmail.com>
2024-04-12 03:17:05 -0700

    commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,29 +1,31 @@
+#include <stdio.h>
 #include "hash_tables.h"
+
 /**
- * hash_table_print - Prints a hash table
- * @ht: The hash table to print
- */
+* hash_table_print - Prints a hash table
+* @ht: The hash table to print
+*/
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
-	int first = 1; /*Flag to track if it is the first element printed*/
-	hash_table_t *node;
+unsigned long int i;
+int first = 1; /* Flag to track if it is the first element printed */
 
-	if (ht == NULL)
-		return;
+if (ht == NULL)
+return;
 
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		*node = ht->array[i];
-		while (node != NULL)
-		{
-			if (!first)
-				print(",");
-			printf("'%s' : '%s'", node->key, node->value);
-			first = 0;
-			node = node->next;
-		}
-	}
-	printf("}\n");
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+hash_node_t *node = ht->array[i];
+while (node != NULL)
+{
+if (!first)
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);
+first = 0;
+node = node->next;
 }
+}
+printf("}\n");
+}
+
