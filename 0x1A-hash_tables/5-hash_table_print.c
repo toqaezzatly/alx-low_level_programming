279c8f44027a25fbf6a43c2329c5aa340a363288
root <toqaezzatly@gmail.com>
2024-04-12 03:13:47 -0700

    commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,29 @@
-#include "hash_tables.h"\n
+#include "hash_tables.h"
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int first = 1; /*Flag to track if it is the first element printed*/
+	hash_table_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		*node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				print(",");
+			printf("'%s' : '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
